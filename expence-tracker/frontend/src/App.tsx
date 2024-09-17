c397f0608750746044372ce23df42ce9e7256a2d
Ali Raza <aaliraza007@hotmail.com>
2024-09-18 00:15:28 +0500

    add successfully shadcn UI

--- expence-tracker/frontend/src/App.tsx
+++ expence-tracker/frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl ">
+    <h1 className="text-3xl bg-green-900 ">
       Hello world! this is 
+      <Button variant={'destructive'}>Click Me</Button>
+
     </h1>
   )
 }
