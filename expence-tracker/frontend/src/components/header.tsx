56c11d6aa8a39709766110048e072a5a0013e8a8
Ali Raza <aaliraza007@hotmail.com>
2024-09-29 22:03:50 +0500

    successfully add logout functionality

--- expence-tracker/frontend/src/components/header.tsx
+++ expence-tracker/frontend/src/components/header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { CircleUser, Landmark, Menu, Package2, Search } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { CircleUser, Landmark, Menu, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 import {
@@ -13,86 +13,128 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-
+import toast, { Toaster } from "react-hot-toast";
+import "react-toastify/dist/ReactToastify.css";
 export function Header() {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    navigate("/login");
+    toast.success("Logged out successfully");
+  };
   return (
     <div className="flex min-h-screen flex-col ">
-    <header className="w-full bg-slate-400/10 flex h-16 items-center gap-2 border-b px-4 md:px-2">
-      <nav className="hidden w-full flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-        {/* <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base"> */}
+      <header className="w-full bg-slate-400/10 flex h-16 items-center gap-2 border-b px-4 md:px-2">
+        <nav className="hidden w-full flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          {/* <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base"> */}
           <Landmark className="h-6 w-6" />
           My-Finance
-        {/* </Link> */}
-        <Link to="/overview" className="text-muted-foreground transition-colors hover:text-foreground">
-          Dashboard
-        </Link>
-        <Link to="/transactions" className="text-muted-foreground transition-colors hover:text-foreground">
-          Transactions
-        </Link>
-        <Link to="/account" className="text-muted-foreground transition-colors hover:text-foreground">
-          Accounts
-        </Link>
-        <Link to="/setting" className="text-foreground transition-colors hover:text-foreground">
-          Settings
-        </Link>
-      </nav>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <nav className="grid gap-6 text-lg font-medium">
-            <Link to="#" className="flex items-center gap-2 text-lg font-semibold">
-            <Landmark className="h-6 w-6" />
-          My-Finance
-            </Link>
-            <Link to="#" className="text-muted-foreground hover:text-foreground">
-              Dashboard
-            </Link>
-            <Link to="#" className="text-muted-foreground hover:text-foreground">
-              Orders
-            </Link>
-            <Link to="#" className="text-muted-foreground hover:text-foreground">
-              Products
-            </Link>
-            <Link to="#" className="text-muted-foreground hover:text-foreground">
-              Customers
-            </Link>
-            <Link to="#" className="hover:text-foreground">Settings</Link>
-          </nav>
-        </SheetContent>
-      </Sheet>
-      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <form className="ml-auto flex-1 sm:flex-initial">
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search products..."
-              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-            />
-          </div>
-        </form>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" size="icon" className="rounded-full">
-              <CircleUser className="h-5 w-5" />
-              <span className="sr-only">Toggle user menu</span>
+          {/* </Link> */}
+          <Link
+            to="/overview"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Dashboard
+          </Link>
+          <Link
+            to="/transactions"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Transactions
+          </Link>
+          <Link
+            to="/account"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Accounts
+          </Link>
+          <Link
+            to="/setting"
+            className="text-foreground transition-colors hover:text-foreground"
+          >
+            Settings
+          </Link>
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
             </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </header>
-  </div>
-  
+          </SheetTrigger>
+          <SheetContent side="left">
+            <nav className="grid gap-6 text-lg font-medium">
+              <Link
+                to="#"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
+                <Landmark className="h-6 w-6" />
+                My-Finance
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Dashboard
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Orders
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Products
+              </Link>
+              <Link
+                to="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Customers
+              </Link>
+              <Link to="#" className="hover:text-foreground">
+                Settings
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+          <form className="ml-auto flex-1 sm:flex-initial">
+            <div className="relative">
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search products..."
+                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+              />
+            </div>
+          </form>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={logout}>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </header>
+    </div>
   );
 }
