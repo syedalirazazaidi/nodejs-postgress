146ee69a1181673f98259f2186edd2f958615c78
Ali Raza <aaliraza007@hotmail.com>
2024-09-30 15:41:06 +0500

    add cards

---
+++ expence-tracker/frontend/src/components/accountAll.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export const description =
+  "An orders dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. The main area has a list of recent orders with a filter and export button. The main area also has a detailed view of a single order with order details, shipping information, billing information, customer information, and payment information.";
+
+export function AccountAll() {
+  return (
+    <div className="flex flex-col sm:gap-4 sm:py-4 mt-4">
+      <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+        <Card x-chunk="dashboard-05-chunk-1">
+          <CardHeader className="pb-2">
+            <CardDescription>This Week</CardDescription>
+            <CardTitle className="text-4xl">$1,329</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-xs text-muted-foreground">
+              +25% from last week
+            </div>
+          </CardContent>
+          <CardFooter>fafs</CardFooter>
+        </Card>
+        <Card x-chunk="dashboard-05-chunk-2">
+          <CardHeader className="pb-2">
+            <CardDescription>This Month</CardDescription>
+            <CardTitle className="text-4xl">$5,329</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-xs text-muted-foreground">
+              +10% from last month
+            </div>
+          </CardContent>
+          <CardFooter>dsfd</CardFooter>
+        </Card>
+      </main>
+    </div>
+  );
+}
--- expence-tracker/frontend/src/pages/account.tsx
+++ expence-tracker/frontend/src/pages/account.tsx
@@ -1,11 +1,20 @@
-import { Header } from '@/components/header'
-import React from 'react'
+import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Plus } from "lucide-react";
+import { AccountAll } from "@/components/accountAll";
 
 export default function Account() {
-    return (
-        <div>
-            <Header/>
-            account
+  return (
+    <div>
+      <Header />
+      <div className="flex  justify-between mt-4">
+        <p className="text-lg font-semibold">Accounts Information</p>
+        <div className="bg-slate-400 py-1 px-7 rounded-md hover:cursor-pointer">
+          <Plus onClick={() => alert("hi developer")} />
         </div>
-    )
+      </div>
+      <AccountAll/>
+    </div>
+  );
 }
